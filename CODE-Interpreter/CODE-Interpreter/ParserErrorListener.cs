32fdd7377419bbbe84efd222a2d9a14a4ecba777
Carl Sebastian Yebes <carlsebastian.yebes@cit.edu>
2023-04-16 23:31:25 +0800

    Modify Error Listener

--- CODE-Interpreter/CODE-Interpreter/ParserErrorListener.cs
+++ CODE-Interpreter/CODE-Interpreter/ParserErrorListener.cs
@@ -3,18 +3,9 @@ namespace CODE_Interpreter;
 
 public class ParserErrorListener : BaseErrorListener, IAntlrErrorListener<IToken>
 { 
-    public void SyntaxError(IRecognizer recognizer, IToken offendingSymbol, int line, int charPositionInLine, string msg, RecognitionException e)
+    public override void SyntaxError(IRecognizer recognizer, IToken offendingSymbol, int line, int charPositionInLine, string msg, RecognitionException e)
     {
-        if (offendingSymbol == null)
-        {
-            // Handle the case where the offending token is null
-            Console.Error.WriteLine($"Token recognition error at line {line}:{charPositionInLine} - {msg}");
-        }
-        else
-        {
-            string tokenName = recognizer.Vocabulary.GetDisplayName(offendingSymbol.Type);
-            string errorMessage = $"Token recognition error at line {line}:{charPositionInLine} - {msg}";
-            Console.Error.WriteLine(errorMessage);
-        }
+        string errorMessage = $"Token recognition error at line {line}:{charPositionInLine} - {msg}";
+        Console.Error.WriteLine(errorMessage);
     }
 }
